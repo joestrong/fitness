2b2e1c6ac97254ab2b568c8d71c247ffb89fcc45
Joe Strong <joestrong@gmail.com>
2016-01-22 22:01:41 +0000

    Fixing bug in refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 echo "Copying repo"
 docker run --volumes-from=fitness-data -v $(pwd):/source debian:jessie cp -r /source/. /web/
 echo "Setting permissions"
-docker run --volumes-from=joestrong-data -w /web debian:jessie chmod -R 755 .
+docker run --volumes-from=fitness-data -w /web debian:jessie chmod -R 755 .
 echo "Refreshed!"
